16dcd52eb58e9951f2bdf91cfa0d4ad24840b57c
hrutik2 <hrutik0729@gmail.com>
2025-01-30 08:22:23 +0530

    done

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import AddReferral from './page/addReferal'
 import Login from './page/login'
 import Signup from './page/signup'
 import { Navbar } from './component/Navbar'
 import { AllRoutes } from './component/AllRoutes'
+import { Test } from './component/test'
 
 
 function App() {
- 
+ const[show,setshow]=useState(true)
+
+
 
   return (
     <div>
-     <Navbar/>
-     <AllRoutes/>
+      
+     <Test />
+    
+
     </div>
   )
 }
---
+++ frontend/my-react-app/src/component/test.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { useState ,useEffect} from "react";
+
+const  url = "https://fakestoreapi.com/products"
+ 
+export const Test=()=>{
+   
+    return(
+        <div style={{width:"100%",height:"100vh",lineHeight:"100vh",textAlign:"center"}}>
+            <div style={{width:"400px",height:"400px",backgroundColor:"red",verticalAlign:"m"}}></div>
+        </div>
+    )
+}
